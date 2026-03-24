5dc6b741b124ab42b3bdd86dbb8882c9bab79deb
ElTioBrian <bri.bravom@duocuc.cl>
2026-03-24 12:12:56 -0300

    controller

---
+++ src/main/java/com/example/bibliotecaduoc/controller/LibroController.java
@@ -0,0 +1,27 @@
+package com.example.bibliotecaduoc.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.bibliotecaduoc.model.Libro;
+import com.example.bibliotecaduoc.service.LibroService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/api/v1/libros80.89")
+public class LibroController {
+    @Autowired
+    private LibroService libroService;
+
+    @GetMapping
+    public List<Libro> listaLibros(){
+        return libroService.readAll();
+    }
+
+}
+
+
