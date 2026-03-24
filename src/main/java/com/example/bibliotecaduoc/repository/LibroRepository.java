d49c4fa7d954fa6cb7dcb966a24a3478cd98f7ca
ElTioBrian <bri.bravom@duocuc.cl>
2026-03-24 11:25:40 -0300

    Repositorio.java completo

--- src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
+++ src/main/java/com/example/bibliotecaduoc/repository/LibroRepository.java
@@ -12,5 +12,44 @@ public class LibroRepository {
 
     private List<Libro> listaLibros = new ArrayList<>();
     
-    
-}
+    public List<Libro> obtenerLibros(){;
+        return listaLibros;
+        }
+
+    public Libro buscarPorId(int id){
+    for (Libro libro : listaLibros) {
+        if (libro.getId() == id) {
+            return libro;
+        }
+    }
+    return null;
+        }
+
+    public Libro buscarPorIsbn(String isbn){
+    for (Libro libro : listaLibros) {
+        if (libro.getIsbn() == isbn) {
+            return libro;
+        }
+    }
+    return null;
+        }
+    public Libro Guardar(Libro libro){
+        listaLibros.add(libro);
+        return libro;
+    }
+    public Libro actualizar(Libro libro){
+        Libro libroBuscado = buscarPorId(libro.getId());
+        if(libroBuscado != null){
+            libroBuscado.setIsbn(libro.getIsbn());
+            libroBuscado.setAutor(libro.getAutor());
+            libroBuscado.setEditorial(libro.getEditorial());
+            libroBuscado.setFechaPublicacion(libro.getFechaPublicacion());
+            libroBuscado.setTitulo(libro.getTitulo());
+            return libroBuscado;
+        }
+        return null;
+    }
+    public void eliminar(int id){
+        listaLibros.removeIf(libro -> libro.getId()== id);
+    }
+} 
